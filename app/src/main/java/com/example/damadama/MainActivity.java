24623c6e955bbafd616eaa67e120896d37d0f99e
ksoyeon621 <ksoyeon621@naver.com>
2017-09-01 11:59:34 +0900

    #2 progressBar set

--- app/src/main/java/com/example/damadama/MainActivity.java
+++ app/src/main/java/com/example/damadama/MainActivity.java
@@ -4,21 +4,32 @@ import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageView main_ani,aways;
     ImageButton but_love, but_food, but_hi;
     AnimationDrawable ani;
+    ProgressBar hungry,like;
+    Button reset;
+    int full=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         main_ani=(ImageView)findViewById(R.id.main_ani);
+        hungry=(ProgressBar)findViewById(R.id.progress_hungry);
+        reset=(Button)findViewById(R.id.reset);
         ani=(AnimationDrawable)main_ani.getDrawable();
     }
-
+    public void resetClick(View v){
+        full=0;
+        hungry.setProgress(full);
+    }
     public void OnClick(View v) {
         switch(v.getId()) {
             case R.id.love:
@@ -29,11 +40,17 @@ public class MainActivity extends AppCompatActivity {
                 ani.start();
                 break;
             case R.id.food:
-                main_ani.setVisibility(View.VISIBLE);
-                if(ani.isRunning()) ani.stop();
-                main_ani.setImageResource(R.drawable.food_ani);
-                ani=(AnimationDrawable)main_ani.getDrawable();
-                ani.start();
+                full+=20;
+                if(full<=100){
+                    hungry.setProgress(full) ;
+                    main_ani.setVisibility(View.VISIBLE);
+                    if(ani.isRunning()) ani.stop();
+                    main_ani.setImageResource(R.drawable.food_ani);
+                    ani=(AnimationDrawable)main_ani.getDrawable();
+                    ani.start();
+                }else{
+                    Toast.makeText(getApplicationContext(), "00(이)가 배가 불러요!", Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.hi:
                 main_ani.setVisibility(View.VISIBLE);
